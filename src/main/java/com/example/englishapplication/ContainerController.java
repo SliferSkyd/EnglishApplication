dbfe3414fa86583d74080612ab672ea91b679879
SliferSkyd <22021108@vnu.edu.vn>
2023-09-29 08:47:01 +0700

    Add dependencies and Fix a bug related to WebView

--- src/main/java/com/example/englishapplication/ContainerController.java
+++ src/main/java/com/example/englishapplication/ContainerController.java
@@ -1,5 +1,28 @@
 package com.example.englishapplication;
 
-public class ContainerController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class ContainerController implements Initializable {
+    @FXML
+    private AnchorPane searchPane;
+    @FXML
+    private SearchController searchController;
+    @FXML
+    private AnchorPane mainContent;
+    @FXML
+    private Button searchButton;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.example.englishapplication {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.graphics;
 
     opens com.example.englishapplication to javafx.fxml;
     exports com.example.englishapplication;
