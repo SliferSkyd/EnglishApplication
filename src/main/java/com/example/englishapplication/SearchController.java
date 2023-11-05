424aa9c74fec86aa3d817b130c7f62eaa730dab1
SliferSkyd <22021108@vnu.edu.vn>
2023-11-05 17:02:33 +0700

    edit style for search box

--- src/main/java/com/example/englishapplication/SearchController.java
+++ src/main/java/com/example/englishapplication/SearchController.java
@@ -42,19 +42,33 @@ public class SearchController extends BaseController implements Initializable {
         listView.setItems(listWords);
         JSONObject meaning = DictionaryManagement.Search(prefix);
         if (meaning == null) {
-            HBox header = new HBox();
-            header.getChildren().add(new Label(prefix));
-
-            definitionView.getChildren().add(header);
             starButton.setVisible(false);
+            String suggestion = FuzzySearch.getCorrectWord(prefix);
+            if (suggestion != null) {
+                HBox header = new HBox();
+                Label label = new Label("Did you mean: " + suggestion);
+                label.getStyleClass().add("word-definition-label");
+                header.getChildren().add(label);
+                definitionView.getChildren().add(header);
+            }
         } else {
             starButton.setVisible(true);
+            HBox header = new HBox();
+            header.setSpacing(5);
+            Label label = new Label(prefix);
+            label.getStyleClass().add("word-definition-label");
+            header.getChildren().add(label);
+
+            label = new Label(String.format("[%s]", meaning.getString("pronoun")));
+            label.getStyleClass().add("word-pronoun-label");
+            header.getChildren().add(label);
+            definitionView.getChildren().add(header);
+
             if (favoriteWords.existWord(prefix)) {
                 star.setImage(starActive.getImage());
             } else {
                 star.setImage(starInactive.getImage());
             }
-
             buildTree(meaning.getJSONArray("type"), 0);
         }
     }
@@ -111,17 +125,28 @@ public class SearchController extends BaseController implements Initializable {
         }
     }
 
+    private Label setStyleLabel(Label label, int depth) {
+        if (depth == 0) {
+            label.getStyleClass().add("word-type-label");
+        } else if (depth == 1) {
+            label.getStyleClass().add("word-explain-label");
+        } else if (depth == 2) {
+            label.getStyleClass().add("word-example-label");
+        }
+        label.setWrapText(true);
+        return label;
+    }
     private void buildTree(JSONArray array, int depth) {
         for (int i = 0; i < array.length(); ++i) {
             JSONObject object = array.getJSONObject(i);
             String name = object.keys().next();
             if (object.get(name) instanceof JSONArray) {
                 Label label = new Label("\t".repeat(depth) + name);
-                definitionView.getChildren().add(label);
+                definitionView.getChildren().add(setStyleLabel(label, depth));
                 buildTree(object.getJSONArray(name), depth + 1);
             } else {
                 Label label = new Label("\t".repeat(depth) + name + ": " + object.get(name));
-                definitionView.getChildren().add(label);
+                definitionView.getChildren().add(setStyleLabel(label, depth));
             }
         }
     }
