2281434ed6e8329f8ebeda664d6a5852a733f3e4
flashhhhh <nguyenchithanhpro196@gmail.com>
2023-11-21 09:21:35 +0700

    change st

--- src/main/java/com/example/englishapplication/base/DictionaryManagement.java
+++ src/main/java/com/example/englishapplication/base/DictionaryManagement.java
@@ -67,8 +67,8 @@ public class DictionaryManagement {
     }
 
 
-    static final String IN_PATH = "src/main/resources/WordDictionary/CommandLineData.txt";
-    static final String OUT_PATH = "src/main/resources/WordDictionary/data.txt";
+    static final String IN_PATH = "src/main/resources/com/example/englishapplication/data/CommandLineData.txt";
+    static final String OUT_PATH = "src/main/resources/com/example/englishapplication/data/data.txt";
     public static String add(String target, String explain) {
         //Dictionary.wordList.add(new Word(target, explain));
         return Dictionary.trie.addWord(target, explain);
